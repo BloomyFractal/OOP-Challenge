14adfe3b387291206e02a2b01ca567bb4f4e66c0
BloomyFractal <bloomy.fract@hotmail.com>
2022-03-05 16:03:34 +0100

    Title Screen Arrow Movement
    Started to Implement Keyboard Control of the Title Screen Arrow.

--- OOP-Challenge/Assets/Scripts/TitleUI.cs
+++ OOP-Challenge/Assets/Scripts/TitleUI.cs
@@ -4,21 +4,39 @@ using UnityEngine;
 
 public class TitleUI : MonoBehaviour
 {
-  public GameObject navArrow;
+  //Arrow variables
+  public Transform navArrow;
+
+  private Vector3 arrowFacesStart = new Vector3(1.1f,2f,0);
+  private Vector3 arrowFacesHowToPlay = new Vector3(1f,1.3f,0);
+  private Vector3 arrowFacesOptions = new Vector3(1f,0.6f,0);
+  private Vector3 arrowFacesQuit = new Vector3(1f,-0.1f,0);
+
+  private Vector3 arrowMove = new Vector3(0,0.7f,0);
 
   //Title Screen Buttons
-  public GameObject startButton;
-  public GameObject howToButton;
-  public GameObject optionsButton;
-  public GameObject quitButton;
+  public GameObject start;
+  public GameObject howToPlay;
+  public GameObject options;
+  public GameObject quit;
 
     void Start()
     {
-
+     navArrow.position = arrowFacesStart;
     }
 
     void Update()
     {
+     ArrowFunctions();
+    }
+
+    private void ArrowFunctions()
+    {
+     //Go downward
+     if (navArrow.position.y != arrowFacesQuit.y && Input.GetKeyDown(KeyCode.DownArrow))
+     {
+       navArrow.position -= arrowMove;
+     }
 
     }
 }
