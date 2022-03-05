c33ee6a03a15810fe9c992fe20b05d4a1e8c62d2
BloomyFractal <bloomy.fract@hotmail.com>
2022-03-05 15:05:21 +0100

    Scripts Folder and TitleUI script
    Made a Scripts folder, and started a TitleUI script, to allow navigation within the Title Screen with keyboard.

---
+++ OOP-Challenge/Assets/Scripts/TitleUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleUI : MonoBehaviour
+{
+  public GameObject navArrow;
+
+  //Title Screen Buttons
+  public GameObject startButton;
+  public GameObject howToButton;
+  public GameObject optionsButton;
+  public GameObject quitButton;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
