00c48b76d4641bb48fbc316ad7570f627591de4d
BloomyFractal <bloomy.fract@hotmail.com>
2022-03-07 11:24:24 +0100

    Started Options Script
    Beginning of the "OptionsUI" script, which will handle the difficulty setting.

---
+++ OOP-Challenge/Assets/Scripts/OptionsUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsUI : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
