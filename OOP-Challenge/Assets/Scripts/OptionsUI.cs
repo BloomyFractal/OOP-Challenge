032688122f58a1acd7a21fd0733202fef0eb956b
BloomyFractal <bloomy.fract@hotmail.com>
2022-03-07 12:09:12 +0100

    Fixed Arrow Z Position
    Navigation arrow in the Options scene is now in front of the panel, thanks to Canvas Component > Render Mode >  Screen Space - Camera.

--- OOP-Challenge/Assets/Scripts/OptionsUI.cs
+++ OOP-Challenge/Assets/Scripts/OptionsUI.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class OptionsUI : MonoBehaviour
 {
+  //Arrow variables
+  private Transform navArrow;
+  private Vector3 arrowMove = new Vector3(0,0.7f,0);
+
+  //Positions close to Options text and buttons
+  private Vector3 arrowFacesDiff = new Vector3(0,5,0);
+
     void Start()
     {
-
+     navArrow = GameObject.Find("NavigationArrow").GetComponent<Transform>();
+     navArrow.position = arrowFacesDiff;
     }
 
     void Update()
