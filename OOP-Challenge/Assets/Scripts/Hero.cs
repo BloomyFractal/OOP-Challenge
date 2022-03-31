b17f26759e8282265b7b4dd2c8619193a3394a4a
BloomyFractal <bloomy.fract@hotmail.com>
2022-03-31 17:50:09 +0200

    Staring Level Scene
    Added Ground + Wall, Level HUD and Started Hero character and script.

---
+++ OOP-Challenge/Assets/Scripts/Hero.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+  //Activate axis
+  private float horiInput;
+
+  //Hero variables
+  private int speed = 20;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+     Move();
+    }
+
+    private void Move()
+    {
+
+    }
+}
