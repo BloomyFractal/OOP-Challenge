2a886f4853957400bc780cac385dbb1326d216f2
BloomyFractal <bloomy.fract@hotmail.com>
2022-03-31 19:55:16 +0200

    Implemented Jump
    Player can now jump, only when on the ground.

--- OOP-Challenge/Assets/Scripts/Hero.cs
+++ OOP-Challenge/Assets/Scripts/Hero.cs
@@ -8,11 +8,14 @@ public class Hero : MonoBehaviour
   private float horiInput;
 
   //Hero variables
-  private int speed = 20;
+  private Rigidbody heroRb;
+  private int speed = 10;
+  private int jumpForce = 200;
+  private bool isJumping;
 
     void Start()
     {
-
+      heroRb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -22,6 +25,28 @@ public class Hero : MonoBehaviour
 
     private void Move()
     {
+     //Horizontal movement
+     horiInput = Input.GetAxis("Horizontal");
 
+     transform.Translate(Vector3.left * speed * horiInput * Time.deltaTime);
+
+     //Make player jump only when on the ground
+     if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+     {
+       heroRb.AddForce(Vector3.up * jumpForce * Time.deltaTime,ForceMode.Impulse);
+
+       isJumping = true;
+       Debug.Log("isJumping = " + isJumping + ".");
+     }
+    }
+
+    //Check if player is touching ground
+    private void OnCollisionEnter(Collision collision)
+    {
+      if (collision.gameObject.tag == "Ground")
+      {
+        isJumping = false;
+        Debug.Log("isJumping = " + isJumping + ".");
+      }
     }
 }
